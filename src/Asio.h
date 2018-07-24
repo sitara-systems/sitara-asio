b043439c19e9e4abce3502cb61cba06e505c8148
nathan lachenmyer <info@morphogen.cc>
2018-07-24 13:58:20 -0400

    changed namespaces

--- src/Asio.h
+++ src/Asio.h
@@ -3,8 +3,3 @@
 #define ASIO_STANDALONE
 #define ASIO_HAS_STD_ATOMIC
 #define ASIO_HAS_STD_TYPE_TRAITS
-
-#include "UdpReceiver.h"
-#include "UdpSender.h"
-#include "TcpServer.h"
-#include "TcpClient.h"
